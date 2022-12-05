c8774f1ce72e262eadcf79e2fb379d1b83c68ac5
Tristan <dodubassman@gmail.com>
2022-12-05 23:33:42 +0100

    ThirdPersonMovement refacto + fix "flying" character

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -14,16 +14,16 @@ public class ThirdPersonMovement : MonoBehaviour
 
     public Transform cam;
     
-    public float speed = 10;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3;
-    Vector3 velocity;
-    bool isGrounded;
-    bool isJumping;
-    bool isRunning;
+    public float speed;
+    private float gravity = -9.81f;
+    public float jumpHeight = 20f;
+    public Vector3 velocity;
+    public bool isGrounded;
+    public bool isJumping;
+    public bool isRunning;
 
     public Transform groundCheck;
-    public float groundDistance = 0.1f;
+    private float groundDistance = 0.1f;
     public LayerMask groundMask;
 
     float turnSmoothVelocity;
@@ -32,7 +32,7 @@ public class ThirdPersonMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
 	
     }
 
@@ -40,7 +40,6 @@ public class ThirdPersonMovement : MonoBehaviour
     void Update()
     {
         
-        //jump
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
@@ -50,32 +49,53 @@ public class ThirdPersonMovement : MonoBehaviour
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            isJumping = true;
         }
-        
-        //gravity
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+       
         
         //walk
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        Move(direction);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
-        }
+
+
+        
         
         animator.SetBool("isRunning", vertical !=0);
 
         animator.SetBool("isJumping", !controller.isGrounded);
         
+    }
+
+    void Move(Vector3 _direction)
+    {
+        if (_direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+
+        if (isJumping)
+        {
+            // Add jump force
+            velocity.y = Mathf.Sqrt(-jumpHeight * gravity);
+            isJumping = false;
+        }
+
+        // Accelerating to the ground
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+
     }
 }
 
