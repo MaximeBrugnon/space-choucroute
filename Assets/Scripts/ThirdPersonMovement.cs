edcbdd968ac820848277d1defac7a0269828f937
Tristan <dodubassman@gmail.com>
2022-12-07 23:11:43 +0100

    Choucroute Movements refacto

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -9,44 +9,80 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
 
+    /**
+     * Choucroute Character controller
+     */
     public CharacterController controller;
+
+    /**
+     * Choucroute's Animator. Avaiable variables are:
+     *   - bool isJumping
+     *   - float groundSpeed (from 0.0f to 1.0f)
+     */
     public Animator animator;
 
+    /**
+     * Third person camera
+     */
     public Transform cam;
-    
-    public float speed;
-    private float gravity = -9.81f;
-    public float jumpHeight;
 
-
-    public Vector3 velocity;
-
-
-    public bool isGrounded;
-    public bool isJumping;
-    public bool isRunning;
-
+    /**
+     * Ground detector's stuffs
+     */
     public Transform groundCheck;
-    private float groundDistance = 0.1f;
     public LayerMask groundMask;
+    private const float GROUND_DISTANCE = 0.1f;
 
-    float turnSmoothVelocity;
-    public float turnSmoothTime = 0.1f;
+    /**
+     * Movement factor constants
+     *   - Running SPEED
+     *   - Earth GRAVITY factor
+     *   - JUMP_POWER factor
+     */
+    private const float SPEED = 15.0f;
+    private const float GRAVITY = -9.81f;
+    private const float JUMP_POWER = 4.0f;
 
-    // Start is called before the first frame update
+    /**
+     * Choucroute current vecored velocity
+     */
+    private Vector3 velocity;
+
+    /**
+     * Movement bool conditions
+     */
+    private bool isGrounded;
+    private bool isJumping;
+
+    /**
+     * Dumping stuffs?
+     */
+    private float turnSmoothVelocity;
+    private float turnSmoothTime = 0.1f;
+
+    /**
+     * Vector3 used to save the last character movement. 
+     * Used while jumping to remember the direction of the jump
+     */
+    private Vector3 lastMoveFromGround;
+
+
     void Start()
     {
         animator = GetComponent<Animator>();
 	
     }
 
-    // Update is called once per frame
     void Update()
     {
         
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        isGrounded = Physics.CheckSphere(groundCheck.position, GROUND_DISTANCE, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
 
-        
+
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             isJumping = true;
@@ -70,30 +106,44 @@ public class ThirdPersonMovement : MonoBehaviour
         animator.SetFloat("groundSpeed", _direction.magnitude);
         animator.SetBool("isJumping", !isGrounded);
 
-        if (_direction.magnitude >= 0.1f) {
-
-       
-            float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        if (isGrounded) // Full controll on ground
+        {
+            if (_direction.magnitude >= 0.1f) // While moving / running
+            {
             
+                float targetAngle = Mathf.Atan2(_direction.x, _direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+                // Runing movement
+                lastMoveFromGround = moveDir.normalized * SPEED *Time.deltaTime;
+
+                controller.Move(lastMoveFromGround);
+            } else 
+            {
+                // No mouvement.
+                lastMoveFromGround = new Vector3(0f, 0f, 0f).normalized * SPEED * Time.deltaTime;
+            }
+
+
+        } else // Limited control while on air
+        {            
+             controller.Move(lastMoveFromGround);
         }
 
         if (isJumping)
         {
             // Add jump force
-            velocity.y = Mathf.Sqrt(-jumpHeight * gravity);
+            velocity.y = Mathf.Sqrt(-JUMP_POWER * GRAVITY);
             isJumping = false;
 
         }
 
         // Accelerating to the ground
-        velocity.y += gravity * Time.deltaTime;
+        velocity.y += GRAVITY * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
 
